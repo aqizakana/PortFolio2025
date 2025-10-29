4b46af4e12d3f30cbd246bf9bbcf0d77178d58fa
aqizakana <archifish56@gmail.com>
2025-10-30 00:33:13 +0900

    fix(app): パス

--- app/components/Mesh/City/building.ts
+++ app/components/Mesh/City/building.ts
@@ -1,6 +1,6 @@
-import { Mesh } from '@components/Mesh/mesh';
 import * as THREE from 'three';
 import { mergeGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
+import { Mesh } from '../mesh';
 
 export class Building extends Mesh {
 	constructor() {
